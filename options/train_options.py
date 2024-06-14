1d867158ca80366146f3acada2a37185400cbb65
levente-1 <levente.baljer@gmail.com>
2024-06-14 17:29:08 +0100

    atme

--- options/train_options.py
+++ options/train_options.py
@@ -8,7 +8,7 @@ class TrainOptions(BaseOptions):
         parser.add_argument('--save_latest_freq', type=int, default=1000, help='frequency of saving the latest results')
         parser.add_argument('--save_epoch_freq', type=int, default=200, help='frequency of saving checkpoints at the end of epochs')
         parser.add_argument('--continue_train', action='store_true', help='continue training: load the latest model')
-        parser.add_argument('--epoch_count', type=int, default=1, help='the starting epoch count, we save the model by <epoch_count>, <epoch_count>+<save_latest_freq>, ...')
+        parser.add_argument('--epoch_count', type=int, default=0, help='the starting epoch count, we save the model by <epoch_count>, <epoch_count>+<save_latest_freq>, ...')
         parser.add_argument('--phase', type=str, default='train', help='train, val, test, etc')
         parser.add_argument('--which_epoch', type=str, default='latest', help='which epoch to load? set to latest to use latest cached model')
         parser.add_argument('--niter', type=int, default=500, help='# of iter at starting learning rate')
@@ -22,8 +22,8 @@ class TrainOptions(BaseOptions):
         parser.add_argument('--lr_decay_iters', type=int, default=50, help='multiply by a gamma every lr_decay_iters iterations')
 
         parser.add_argument('--super_train', type=int, default=1, help='start with supervised training (0 if we go straight to unsupervised)')
-        parser.add_argument('--super_epoch', type=int, default=250, help='number of epochs for supervised training ')
-        parser.add_argument('--labelSmooth', type=int, default=0, help='indicate to use label smoothing (0 if no label smoothing)')
+        parser.add_argument('--super_epoch', type=int, default=0, help='number of epochs for supervised training ')
+        parser.add_argument('--labelSmooth', type=int, default=1, help='indicate to use label smoothing (0 if no label smoothing)')
 
         self.isTrain = True
         return parser
