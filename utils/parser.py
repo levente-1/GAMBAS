94916d57a152dfcef24f1abd822d52abbbcf01b4
nialljb <nialljbourke@gmail.com>
2025-02-14 14:55:37 +0000

    Setting up FW dependencies

---
+++ utils/parser.py
@@ -0,0 +1,54 @@
+"""Parser module to parse gear config.json."""
+
+from typing import Tuple
+import os
+import re
+from flywheel_gear_toolkit import GearToolkitContext
+
+def parse_config(context):
+    """Parse the config and other options from the context, both gear and app options.
+
+    Returns:
+        gear_inputs
+        gear_options: options for the gear
+        app_options: options to pass to the app
+    """
+
+    # Get the input file id
+    input_container = context.client.get_analysis(context.destination["id"])
+
+    # Get the subject id from the session id
+    # & extract the subject container
+    subject_id = input_container.parents['subject']
+    subject_container = context.client.get(subject_id)
+    subject = subject_container.reload()
+    print("subject label: ", subject.label)
+    subject_label = subject.label
+
+    # Get the session id from the input file id
+    # & extract the session container
+    session_id = input_container.parents['session']
+    session_container = context.client.get(session_id)
+    session = session_container.reload()
+    session_label = session.label
+    session_label = session_label.replace(" ", "_")
+    print("session label: ", session.label)
+    
+
+    # -------------------  Get Acquisition label -------------------  #
+
+    # Specify the directory you want to list files from
+    directory_path = '/flywheel/v0/input/input'
+    # List all files in the specified directory
+    for filename in os.listdir(directory_path):
+        if os.path.isfile(os.path.join(directory_path, filename)):
+            filename_without_extension = filename.split('.')[0]
+            no_white_spaces = filename_without_extension.replace(" ", "")
+            # no_white_spaces = filename.replace(" ", "")
+            cleaned_string = re.sub(r'[^a-zA-Z0-9]', '_', no_white_spaces)
+            input_label = cleaned_string.rstrip('_') # remove trailing underscore
+
+    print("Input label: ", input_label)
+
+    # gear_inputs, 
+    return subject_label, session_label, input_label
