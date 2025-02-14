42fc6baa0e82d0d1878ee7d22136293099e9d821
nialljb <nialljbourke@gmail.com>
2025-02-14 14:55:37 +0000

    Setting up FW dependencies

---
+++ docs/changelog.md
@@ -0,0 +1,6 @@
+# Changelog
+
+14/02/2025
+Version 0.0.1
+
+Refactoring for Flywheel gear
---
+++ run.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+"""The run script."""
+import logging
+import os
+import sys
+
+# import flywheel functions
+from flywheel_gear_toolkit import GearToolkitContext
+from utils.parser import parse_config
+
+
+# Add top-level package directory to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# Verify sys.path
+print("sys.path:", sys.path)
+
+# The gear is split up into 2 main components. The run.py file which is executed
+# when the container runs. The run.py file then imports the rest of the gear as a
+# module.
+
+log = logging.getLogger(__name__)
+
+def main(context: GearToolkitContext) -> None:
+    # """Parses config and runs."""
+    subject_label, session_label, input_label = parse_config(context)
+    
+    # Run main function
+    test.py(subject_label, session_label, input_label)
+
+
+# Only execute if file is run as main, not when imported by another module
+if __name__ == "__main__":  # pragma: no cover
+    # Get access to gear config, inputs, and sdk client if enabled.
+    with GearToolkitContext() as gear_context:
+
+        # Initialize logging, set logging level based on `debug` configuration
+        # key in gear config.
+        gear_context.init_logging()
+
+        # Pass the gear context into main function defined above.
+        main(gear_context)
